7b84c1980507fe8a397e3a36452107e78d66825d
issamDjida <i.djida@esi-sba.dz>
2022-12-18 14:18:46 +0100

    Fix General margin

--- src/Layouts/Containers/ClientContainter.jsx
+++ src/Layouts/Containers/ClientContainter.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function ClientContainter({children  , LayoutBackground="" , Custumize="",LayoutBackgroundOverRide ,CustumizeOverRide , FlexerSyle}) {
     const Custum = CustumizeOverRide
                   ? SafeString(CustumizeOverRide)
-                  : `w-full px-3 mx-3 lg:mx-0 sm:px-0 max-w-[700px] lg:max-w-[964px] xl:max-w-[1170px] 3xl:max-w-[1500px] ${SafeString(Custumize)} `
+                  : `w-full px-3 mx-3 lg:mx-0 sm:px-0 max-w-[700px] lg:max-w-[764px] xl:max-w-[1170px] 3xl:max-w-[1500px] ${SafeString(Custumize)} `
 
     const RootContainer = LayoutBackgroundOverRide
                         ? SafeString(LayoutBackgroundOverRide)
